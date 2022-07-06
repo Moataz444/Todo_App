495a0f05abfd47a009fdbcc750b638d6b7481962
Moataz444 <you@example.com>
2022-07-06 09:20:48 +0200

    dropdown button decoration

--- lib/modules/settings_screen.dart
+++ lib/modules/settings_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/shared/styles/colors.dart';
 
+import '../shared/components/constants.dart';
+
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
@@ -10,7 +12,6 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   void popupButtonSelected(String value) {}
-  String dropdownValue = 'Blue Theme';
   Color color = clr;
 
   @override
@@ -22,49 +23,63 @@ class _SettingsScreenState extends State<SettingsScreen> {
           const Text('Themes',
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700)),
           const SizedBox(
-            width: 10,
+            width: 12,
           ),
-          DropdownButton<String>(
-            value: dropdownValue,
-            style: const TextStyle(color: Colors.black, fontSize: 20),
-            items: const [
-              DropdownMenuItem(
-                value: 'Blue Theme',
-                child: Text('Blue Theme'),
+          Container(
+            padding: const EdgeInsetsDirectional.only(start: 7, end: 7),
+            width: 190,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(color: clr, width: 4)),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                isExpanded: true,
+                iconSize: 35,
+                value: dropdownValue,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+                items: const [
+                  DropdownMenuItem(
+                    value: 'Blue Theme',
+                    child: Text('Blue Theme'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Red Theme',
+                    child: Text('Red Theme'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Pink Theme',
+                    child: Text('Pink Theme'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Green Theme',
+                    child: Text('Green Theme'),
+                  ),
+                ],
+                onChanged: (value) {
+                  switch (value) {
+                    case ('Blue Theme'):
+                      color = blue;
+                      break;
+                    case ('Red Theme'):
+                      color = red;
+                      break;
+                    case ('Pink Theme'):
+                      color = pink;
+                      break;
+                    case ('Green Theme'):
+                      color = green;
+                      break;
+                  }
+                  setState(() {
+                    dropdownValue = value.toString();
+                    clr = color;
+                  });
+                },
               ),
-              DropdownMenuItem(
-                value: 'Red Theme',
-                child: Text('Red Theme'),
-              ),
-              DropdownMenuItem(
-                value: 'Pink Theme',
-                child: Text('Pink Theme'),
-              ),
-              DropdownMenuItem(
-                value: 'Green Theme',
-                child: Text('Green Theme'),
-              ),
-            ],
-            onChanged: (value) {
-              switch (value) {
-                case ('Blue Theme'):
-                  color = blue;
-                  break;
-                case ('Red Theme'):
-                  color = red;
-                  break;
-                case ('Pink Theme'):
-                  color = pink;
-                  break;
-                case ('Green Theme'):
-                  color = green;
-                  break;
-              }
-              setState(() {
-                dropdownValue = value.toString();
-                clr = color;
-              });
-            },
+            ),
           ),
         ],
       ),
---
+++ lib/shared/components/constants.dart
@@ -0,0 +1,1 @@
+String? dropdownValue = 'Blue Theme';
